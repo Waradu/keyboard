bc01bd43baac6bb845ecc4c909cdb4644260ec71
pandadev <70103896+0PandaDEV@users.noreply.github.com>
2025-03-04 16:09:59 +0100

    chore: set up Nuxt playground with basic configuration

---
+++ nuxt/app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div>{{ message }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { keyboard, Key } from '../src/index';
+const message = ref('');
+
+keyboard.init();
+
+keyboard.down([Key.A], () => {
+  message.value = 'A key pressed';
+});
+
+keyboard.down([Key.LeftShift, Key.B], () => {
+  message.value = 'Shift + B pressed';
+});
+
+keyboard.prevent.up([Key.LeftControl, Key.C], () => {
+  message.value = 'Ctrl + C prevented';
+});
+</script>
